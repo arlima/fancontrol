3429bf1a0afa008e2d9b658900c2c14fab46dba5
arlima <adriano.lima@arlima.com.br>
2016-11-28 20:15:35 -0200

    Add files via upload

--- termostato.py
+++ termostato.py
@@ -6,15 +6,27 @@ import signal
 import sys
 
 def handleTERM(signal, frame):
+    print('Stopping...')
+    p.stop()
     GPIO.cleanup()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, handleTERM)
+signal.signal(signal.SIGTERM, handleTERM)
+signal.signal(signal.SIGHUP, handleTERM)
+
 
 pin = 7
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin,GPIO.OUT)
+p = GPIO.PWM(pin, 100)
+p.start(0)
+temp_max = 60.0
+temp_min = 35.0
+dc_min = 20
+dc_max = 100
+temp = 0.0
 
 while True:
 	f = open ('/sys/class/thermal/thermal_zone0/temp','r')
@@ -22,13 +34,16 @@ while True:
 	#print (r)
 	temp = float(r)/1000.0
 	#print(temp)
-	if temp > 55.0:
-		GPIO.output(pin,GPIO.HIGH)
-		#print('Temperature is', temp, '. Fan is ON')
-	if temp < 45.0:
-		GPIO.output(pin,GPIO.LOW)
-		#print('Temperature is', temp, '. Fan is OFF')
+	if temp > temp_max:
+		dc = dc_max
+	elif temp < temp_min:
+		dc = dc_min
+	else:
+		dc = (1-(temp_max - temp)/(temp_max - temp_min))*(dc_max-dc_min)+dc_min
+	#print(temp,',', dc)
+	p.ChangeDutyCycle(dc)
 	f.close()
-	time.sleep(10)
+	time.sleep(1)
 
+p.stop()
 GPIO.cleanup()
